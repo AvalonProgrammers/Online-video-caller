4c3e32915a588a76aa08e09e08479075e05c1cad
DrSparky-2007 <adityaiyer2007@gmail.com>
2021-01-10 20:21:48 +0530

    Many MANY tests

--- caller/main.py
+++ caller/main.py
@@ -1,2 +1,14 @@
 import cv2
-print(cv2.__version__)
+print("Running opencv", cv2.__version__)
+cap = cv2.VideoCapture(0)
+
+ret, img = cap.read()
+simplified = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+print(
+    len(simplified),
+    len(simplified[0])
+)
+cv2.imshow('frame', simplified)
+
+cap.release()
+cv2.destroyAllWindows()
