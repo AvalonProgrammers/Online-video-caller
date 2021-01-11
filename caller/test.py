a69e1e4ed27121de00fe0821cb57d9cfd7de70b9
DrSparky-2007 <adityaiyer2007@gmail.com>
2021-01-11 08:34:10 +0530

    Renamed main -> test

--- caller/test.py
+++ caller/test.py
@@ -1,14 +1,22 @@
 import cv2
+
+
 print("Running opencv", cv2.__version__)
 cap = cv2.VideoCapture(0)
 
 ret, img = cap.read()
-simplified = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# Up-down, then side to side
+# 
+# p1 p2 p3
+# p1 p2 p3
+# p1 p2 p3
+
 print(
-    len(simplified),
-    len(simplified[0])
+    len(img),
+    len(img[0])
 )
-cv2.imshow('frame', simplified)
+cv2.imshow('frame', img)
 
 cap.release()
 cv2.destroyAllWindows()
