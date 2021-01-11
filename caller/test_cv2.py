5c9fcbf10aef0951c9b330565f6653baf7c84cf8
DrSparky-2007 <adityaiyer2007@gmail.com>
2021-01-11 10:11:34 +0530

    Tests
    I'm doing testing so that I can implement things into the final product

--- caller/test_cv2.py
+++ caller/test_cv2.py
@@ -1,9 +1,23 @@
 import cv2
+import numpy as np
+from rgb2hsv import rgb2hsv
 
 print("Running opencv", cv2.__version__)
 cap = cv2.VideoCapture(0)
 
 ret, img = cap.read()
+
+print(
+    len(img),
+    len(img[0])
+)
+
+print(
+np.array([
+    [
+        [channelVal//20 for channelVal in rgb2hsv(*pix)] for pix in col
+    ] for col in img
+]))
 
 # Up-down, then side to side
 # 
@@ -11,10 +25,6 @@ ret, img = cap.read()
 # p1 p2 p3
 # p1 p2 p3
 
-print(
-    len(img),
-    len(img[0])
-)
 cv2.imshow('frame', img)
 
 cap.release()
